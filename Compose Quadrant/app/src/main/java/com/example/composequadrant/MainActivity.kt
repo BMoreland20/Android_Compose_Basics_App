94618a04b5483d7a717a0efa6a35b6e56a013d25
Blaine Moreland <blaine.l.moreland@gmail.com>
2023-03-08 15:24:25 -0600

    Update
    Large code update to .kt file, with execution file updates as well.

--- Compose Quadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ Compose Quadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
@@ -22,7 +26,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ComposeQuadrantApp(stringResource(R.string.Title1), stringResource(R.string.TextComposable), stringResource(R.string.Title2),
+                        stringResource(R.string.ImageComposable), stringResource(R.string.Title3),
+                        stringResource(R.string.RowComposable), stringResource(R.string.Title4),
+                        stringResource(R.string.ColumnComposable))
                 }
             }
         }
@@ -30,14 +37,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeQuadrantApp(Title1: String, TextComposable: String, Title2: String, ImageComposable: String, Title3: String, RowComposable: String,
+                       Title4: String, ColumnComposable: String, modifier: Modifier = Modifier) {
+    Box (
+        modifier = Modifier.fillMaxSize()
+            ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = Title1)
+            Text(
+                text = TextComposable)
+            Text(
+                text = Title2)
+            Text(
+                text = ImageComposable)
+            Text(
+                text = Title3)
+            Text(
+                text = RowComposable)
+            Text(
+                text = Title4)
+            Text(
+                text = ColumnComposable
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ComposeQuadrantAppPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        ComposeQuadrantApp(stringResource(R.string.Title1), stringResource(R.string.TextComposable), stringResource(R.string.Title2),
+            stringResource(R.string.ImageComposable), stringResource(R.string.Title3),
+            stringResource(R.string.RowComposable), stringResource(R.string.Title4),
+            stringResource(R.string.ColumnComposable))
     }
 }
