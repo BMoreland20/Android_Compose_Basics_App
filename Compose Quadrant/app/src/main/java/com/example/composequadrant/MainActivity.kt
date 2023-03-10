fded036a348a5f64216db0dc5c6e62b1ecc7fefe
Blaine Moreland <blaine.l.moreland@gmail.com>
2023-03-10 11:51:18 -0600

    Update MainActivity.kt

--- Compose Quadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ Compose Quadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.composequadrant
 
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -12,8 +13,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
@@ -24,14 +27,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeQuadrantTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    ComposeQuadrantApp(stringResource(R.string.Title1), stringResource(R.string.TextComposable), stringResource(R.string.Title2),
-                        stringResource(R.string.ImageComposable), stringResource(R.string.Title3),
-                        stringResource(R.string.RowComposable), stringResource(R.string.Title4),
-                        stringResource(R.string.ColumnComposable))
+                Surface(color = MaterialTheme.colors.background) {
+                    ComposeQuadrantApp()
                 }
             }
         }
@@ -39,8 +36,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ComposeQuadrantApp(Title1: String, TextComposable: String, Title2: String, ImageComposable: String, Title3: String, RowComposable: String,
-                       Title4: String, ColumnComposable: String, modifier: Modifier = Modifier) {
+fun ComposeQuadrantApp() {
+    Column(
+        Modifier.fillMaxSize()
+    ) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.Title1),
+                description = stringResource(R.string.TextComposable),
+                backgroundColor = Color.Green,
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.Title2),
+                description = stringResource(R.string.ImageComposable),
+                backgroundColor = Color.Yellow,
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.Title3),
+                description = stringResource(R.string.RowComposable),
+                backgroundColor = Color.Cyan,
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.Title4),
+                description = stringResource(R.string.ColumnComposable),
+                backgroundColor = Color.LightGray,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ComposableInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -50,43 +85,21 @@ fun ComposeQuadrantApp(Title1: String, TextComposable: String, Title2: String, I
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = Title1,
-            fontWeight = FontWeight.Bold
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
-            text = TextComposable
+            text = description,
+            textAlign = TextAlign.Justify
         )
-        Text(
-            text = Title2,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = ImageComposable
-        )
-        Text(
-            text = Title3,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = RowComposable)
-
-        Text(
-            text = Title4,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = ColumnComposable
-        )
-        }
     }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun ComposeQuadrantAppPreview() {
     ComposeQuadrantTheme {
-        ComposeQuadrantApp(stringResource(R.string.Title1), stringResource(R.string.TextComposable), stringResource(R.string.Title2),
-            stringResource(R.string.ImageComposable), stringResource(R.string.Title3),
-            stringResource(R.string.RowComposable), stringResource(R.string.Title4),
-            stringResource(R.string.ColumnComposable))
+        ComposeQuadrantApp()
     }
 }
